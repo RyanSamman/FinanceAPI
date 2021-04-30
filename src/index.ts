ac894cef1c1065d2537e9def2f0d6ed6b766e413
RyanSamman <RyanSamman@outlook.com>
2021-04-30 05:28:40 +0300

    Use .env PORT & Logger upon server startup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from 'http'
+import { config, logger } from './util'
 import api from './api'
 
 const server = http.createServer(api)
 
-server.listen(6000, () => console.log('Server Started on port 6000'))
+server.listen(config.PORT, () => logger.info(`Server Started on port ${config.PORT}`))
